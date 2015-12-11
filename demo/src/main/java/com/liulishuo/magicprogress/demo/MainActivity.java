fe665870657cc252c2a1bd4f17ba0affcd7a7124
Jacksgong <jacks@liulishuo.com>
2015-12-11 08:45:35 +0800

    init demo & add basic example

---
+++ demo/src/main/java/com/liulishuo/magicprogress/demo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.liulishuo.magicprogress.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Jacksgong on 12/10/15.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_github:
+                openGitHub();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openGitHub() {
+        // TODO 添加github地址
+//        Uri uri = Uri.parse(getString(R.string.app_github_url));
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        startActivity(intent);
+    }
+}
