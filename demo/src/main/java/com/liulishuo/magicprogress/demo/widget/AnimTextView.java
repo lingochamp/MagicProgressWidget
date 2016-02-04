5e5bbf7d5bf16d44b9df9c6f337616598661428a
Jacksgong <igzhenjie@gmail.com>
2016-02-05 02:52:33 +0800

    test(smooth-progress): add smooth-progress integration to AnimTextView

--- demo/src/main/java/com/liulishuo/magicprogress/demo/widget/AnimTextView.java
+++ demo/src/main/java/com/liulishuo/magicprogress/demo/widget/AnimTextView.java
@@ -6,10 +6,17 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
+
+import cn.dreamtobe.percentsmoothhandler.ISmoothTarget;
+import cn.dreamtobe.percentsmoothhandler.SmoothHandler;
+
 /**
  * Created by Jacksgong on 12/11/15.
  */
-public class AnimTextView extends TextView {
+public class AnimTextView extends TextView implements ISmoothTarget {
+    private SmoothHandler smoothHandler;
+
     public AnimTextView(Context context) {
         super(context);
     }
@@ -28,9 +35,13 @@ public class AnimTextView extends TextView {
     }
 
     private int progress;
-    private int max;
+    private int max = 100;
 
     public void setProgress(final int progress) {
+        if (smoothHandler != null) {
+            smoothHandler.commitPercent(progress / (float) getMax());
+        }
+
         this.progress = progress;
         setText(String.valueOf(progress));
     }
@@ -45,5 +56,24 @@ public class AnimTextView extends TextView {
 
     public void setMax(int max) {
         this.max = max;
+    }
+
+    @Override
+    public float getPercent() {
+        return getProgress() / (float) getMax();
+    }
+
+    @Override
+    public void setPercent(float percent) {
+        setProgress((int) Math.ceil(percent * getMax()));
+    }
+
+    @Override
+    public void setSmoothPercent(float percent) {
+        if (smoothHandler == null) {
+            smoothHandler = new SmoothHandler(new WeakReference<ISmoothTarget>(this));
+        }
+
+        smoothHandler.loopSmooth(percent);
     }
 }
